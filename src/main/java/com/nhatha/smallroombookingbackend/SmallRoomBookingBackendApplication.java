fb7df020b578229cb64a9889fd4babac9791eee1
Nhat Ha <nhathadt11@gmail.com>
2018-03-14 13:24:00 +0700

    Init app with spring boot

---
+++ src/main/java/com/nhatha/smallroombookingbackend/SmallRoomBookingBackendApplication.java
@@ -0,0 +1,12 @@
+package com.nhatha.smallroombookingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallRoomBookingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallRoomBookingBackendApplication.class, args);
+	}
+}
