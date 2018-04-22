c832276ba198d2042a54e53928ea9723582950d9
Nhat Ha <nhathadt11@gmail.com>
2018-04-22 23:50:33 +0700

    Add run scripts on Mac/Linux and Windows

---
+++ run.sh
@@ -0,0 +1,5 @@
+export JDBC_DATABASE_URL="jdbc:postgresql://ec2-54-204-44-140.compute-1.amazonaws.com:5432/d74sbu238k89tg?sslmode=require"
+export JDBC_DATABASE_USERNAME="apjmvdqwsxuorm"
+export JDBC_DATABASE_PASSWORD="1e919e533eefae5a3ea294796911af5dab290dbae52b33b1d5f10d49433e11c7"
+
+mvn spring-boot:run
