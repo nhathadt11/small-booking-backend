f7948816ab3958b3b66380442287434ed9e2af5b
Nhat Ha <nhathadt11@gmail.com>
2018-04-23 00:18:09 +0700

    Remove equality sign in batch file

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
-set JDBC_DATABASE_URL="jdbc:postgresql://ec2-54-204-44-140.compute-1.amazonaws.com:5432/d74sbu238k89tg?sslmode=require"
-set JDBC_DATABASE_USERNAME="apjmvdqwsxuorm"
-set JDBC_DATABASE_PASSWORD="1e919e533eefae5a3ea294796911af5dab290dbae52b33b1d5f10d49433e11c7"
+set JDBC_DATABASE_URL "jdbc:postgresql://ec2-54-204-44-140.compute-1.amazonaws.com:5432/d74sbu238k89tg?sslmode require"
+set JDBC_DATABASE_USERNAME "apjmvdqwsxuorm"
+set JDBC_DATABASE_PASSWORD "1e919e533eefae5a3ea294796911af5dab290dbae52b33b1d5f10d49433e11c7"
 
 mvn spring-boot:run
